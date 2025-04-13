798ab3b4d001981faf23d5f1f52fa1f1b0be2592
PoornimaBaste <mbastembaste@gmail.com>
2025-04-13 21:05:41 +0530

    Set up deploy script and homepage

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -28,10 +28,12 @@ export default function TextForm(props) {
       props.showAlert("Extra spaces removed!","success");
     }
     const handleCopy=()=>{
-      console.log("I am copy");
-      var text=document.getElementById("myBox");
-      text.select();
-      text.setSelectionRange(0,9999);
+      //console.log("I am copy");
+      //var text=document.getElementById("myBox");
+     // text.select();
+      navigator.clipboard.writeText(text);
+      document.getSelection().removeAllRanges();
+      //text.setSelectionRange(0,9999);
       props.showAlert("Copied to Clipboard!","success");
     }
     const handleClearClick=()=>{
@@ -66,7 +68,7 @@ export default function TextForm(props) {
         {/* <p>{wordCount} words and {text.length} characters</p>
         <p>{(0.008*wordCount).toFixed(2)} minutes to read</p> */}
         {/* OR */}
-        <p>{text.split(" ").filter((ele) => ele.length !== 0).length} words and {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((ele) => ele.length !== 0).length} words and {text.length} characters</p>
          <p>{0.008*text.split(" ").filter((ele) => ele.length !== 0).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Nothing to preview!"}</p>
